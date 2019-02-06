e3839ace94f1f90b65cbb0df8c295044af44d871
Diego Fernandez <diego4ng@hotmail.com>
2019-02-05 22:13:16 -0600

    fix view controller coloums

--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -31,7 +31,12 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        // Descomentar para que sean 3 columnas
+        // let width = (view.frame.size.width - 20) / 3
+        //Dos columnas
+        let width = (view.frame.size.width - 20) / 2
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: width, height: width)
 
 }
 }
